c67f98678c087c230164c2f470c5870aeeb7753a
eoinmurray <amonecho1@gmail.com>
2016-08-02 13:26:58 +0200

    coffescript path fixed

--- index.py
+++ index.py
@@ -6,8 +6,9 @@ from bokeh.plotting import figure, output_file, save
 
 Set3_12 = ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']
 
+
 class CustomSaveTool(Tool):
-    __implementation__ = open("custom_save.coffee").read()
+    __implementation__ = open(os.path.join(os.path.dirname(__file__), "custom_save.coffee")).read()
 
 
 def main(filepath):
